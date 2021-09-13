d545936207d6a705666d6f6b7f575c9936ae8486
Aleksandra Kovalenko <al.kovalen@mail.ru>
2021-09-13 17:07:03 +0300

    hometask

---
+++ src/items.js
@@ -0,0 +1,60 @@
+import img1 from "../media/images/1.jpg";
+import img2 from "../media/images/2.jpg";
+import img3 from "../media/images/3.jpg";
+import img4 from "../media/images/4.gif";
+
+import audio1 from "../media/audio/1.mp3";
+import audio2 from "../media/audio/2.mp3";
+import audio3 from "../media/audio/3.mp3";
+
+import video1 from "../media/video/1.mp4";
+import video2 from "../media/video/2.mp4";
+
+export default [
+    {
+        item: img1,
+        type: 'image',
+        title: 'Краски осени'
+    } ,
+    {
+        item: img2,
+        type: 'image',
+        title: 'Осень в горах'
+    } ,
+    {
+        item: img3,
+        type: 'image',
+        title: 'Домики у реки'
+    } ,
+    {
+        item: img4,
+        type: 'image',
+        title: 'Котик в горах'
+    },
+    {
+        item: audio1,
+        type: 'audio',
+        title: 'Пение птиц'
+    },
+    {
+        item: audio2,
+        type: 'audio',
+        title: 'Музыка и звуки ручья'
+    },
+    {
+        item: audio3,
+        type: 'audio',
+        title: 'Звуки леса'
+    },
+    {
+        item: video1,
+        type: 'video',
+        title: 'Песня'
+    },
+    {
+        item: video2,
+        type: 'video',
+        title: 'Море'
+    }
+
+];
